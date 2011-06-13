c89dbc2d3276b16dd1bdc9905621ccaff3874e35
Petr Viktorin <encukou@gmail.com>
2011-06-13 14:16:35 +0200

    Include package data in setup

--- setup.py
+++ setup.py
@@ -21,4 +21,10 @@ setup(
             "cython>=0.14",
         ],
     packages=find_packages(),
+
+    zip_safe=False,
+    include_package_data=True,
+    package_data = {
+            '': ['*.png'],
+        },
 )
